52147ba58976771450322a94d0562da0c65d1944
Sayali Vyavahare <sayali.vyavahare17@gmail.com>
2024-06-29 22:41:16 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const dataRoutes = require('./routes/dataRoutes');
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api', dataRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
